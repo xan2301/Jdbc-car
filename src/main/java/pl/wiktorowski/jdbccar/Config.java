9059face65680a44291ac136dc92a08271d8bc42
xan2301 <2silverghost2@gmail.com>
2023-01-27 11:04:48 +0100

    DataBase22

--- src/main/java/pl/wiktorowski/jdbccar/Config.java
+++ src/main/java/pl/wiktorowski/jdbccar/Config.java
@@ -1,9 +1,12 @@
 package pl.wiktorowski.jdbccar;
 
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
@@ -14,18 +17,34 @@ public class Config {
 
     @Bean
 
-    public DataSource getDataSource()
-
-    {
+    public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url("jdbc:mysql://sql.freedb.tech:3306");
-        dataSourceBuilder.username("freedb_xannn");
-        dataSourceBuilder.password("$DAT$Nfe3GhJCv2");
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("natalia2301");
         dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
         return dataSourceBuilder.build();
 
     }
 
+    @Bean
+
+    public JdbcTemplate getJdbcTemplate() {
+
+        return new JdbcTemplate(getDataSource());
+
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+
+    public void dbinit() {
+
+       String sql = ("CREATE TABLE nazwaTabeli ( ID INT, Imie VARCHAR(200), Nazwisko VARCHAR(200) )");
+
+//hh
+
+        getJdbcTemplate().update(sql);
+    }
 
 
 }
